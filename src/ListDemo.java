6dccf220ea77b439c39c4fe658e9ee00674db56c
Durgesh798 <durgeshwarreddy.perolla@gmail.com>
2022-10-15 23:03:56 -0500

    Commit

---
+++ src/ListDemo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo
+{
+    public static void main(String[] args) {
+        List c = new ArrayList();
+        c.add(2);
+        c.add(5);
+        c.add(1);
+        c.add(1, "dhoni");
+
+        for(Object o:c)
+        {
+            System.out.println(o);
+        }
+
+    }
+}
---
+++ src/SetDemo.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetDemo
+{
+    public static void main(String[] args)
+    {
+        Set<Integer> s=new HashSet<>();
+        s.add(45);
+        s.add(78);
+        s.add(67);
+
+        for(int i:s)
+        {
+            System.out.println(i);
+        }
+
+        System.out.println("---------");
+
+        Set<Integer> s1=new TreeSet<>();
+        s1.add(45);
+        s1.add(78);
+        s1.add(67);
+
+        for(int i:s1)
+        {
+            System.out.println(i);
+        }
+    }
+}
